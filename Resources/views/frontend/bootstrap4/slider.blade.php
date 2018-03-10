566497119f5acfd5a638d408ad0aaf8e186f1b4b
Mariusz Fik <mariusz@fidano.pl>
2018-03-10 21:30:48 +0100

    Wrap carousel inside #carousel-area to improve indicators layout.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- Resources/views/frontend/bootstrap4/slider.blade.php
+++ Resources/views/frontend/bootstrap4/slider.blade.php
@@ -1,7 +1,9 @@
 <div id="{{ $slider->system_name }}" class="carousel slide" data-ride="carousel">
-    @include('slider::frontend.bootstrap4.indicators', ['slider' => $slider])
-    <div class="carousel-inner">
-        @include('slider::frontend.bootstrap4.slides', ['slider' => $slider])
+    <div id="carousel-area">
+        @include('slider::frontend.bootstrap4.indicators', ['slider' => $slider])
+        <div class="carousel-inner">
+            @include('slider::frontend.bootstrap4.slides', ['slider' => $slider])
+        </div>
+        @include('slider::frontend.bootstrap4.controls', ['slider' => $slider])
     </div>
-    @include('slider::frontend.bootstrap4.controls', ['slider' => $slider])
 </div>
